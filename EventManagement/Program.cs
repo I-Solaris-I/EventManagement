bed32f6b1ca4d6d24c0266e353c3fa104cebccd2
Андрей Голубев <golandroser@gmail.com>
2026-08-26 13:08:56 +0300

    Настройка DI для валидатора и репозитория, подключение дополнительных сервисов, настройка конвейера обработки запросов

--- EventManagement/Program.cs
+++ EventManagement/Program.cs
@@ -1,17 +1,53 @@
+using EventManagement.Context;
+using EventManagement.Context.Interfaces;
+using EventManagement.Models;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseDefaultServiceProvider(options =>
+{
+    //Включаем проверку подключаемых зависимостей на цикличность, неверное использование жизненного цикла
+    if (builder.Environment.IsDevelopment())
+    {
+        options.ValidateOnBuild = true;
+        options.ValidateScopes = true;
+    }
+});
+// Толлько для минимальных API
+builder.Services.AddEndpointsApiExplorer();
+// Стандартизация ответов
+builder.Services.AddProblemDetails();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+.ConfigureApiBehaviorOptions(options =>
+{
+    //Отключаем стандартную проверку валидации модели
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+
+
+#warning Для данных в памяти используем Singleton(для многопоточного доступа в InMemoryEvents используется System/Threading.Lock, однако для других реализаций будем использовать Scoped
+builder.Services.AddSingleton<IRepository<Event>, InMemoryEvents>();
+builder.Services.AddTransient<IValidator<CreateUpdateEventDTO>, CreateUpdateEventDTOValidation>();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+if (!app.Environment.IsDevelopment())
+{
+    app.UseStatusCodePages();
+    app.UseHttpsRedirection();
+}
+app.UseRouting();
 app.MapControllers();
 
 app.Run();
+// Не удалять! Без этого тесты не будут работать корректно
+public partial class Program { }
+
